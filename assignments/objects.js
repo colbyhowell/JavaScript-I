9338a1b31653351f1604d285a39986790a6c4137
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-05-22 22:36:00 -0500

    Added all question answers to the three JS files. Only did stretch for the function conversion file

--- assignments/objects.js
+++ assignments/objects.js
@@ -16,29 +16,82 @@ const example = {
   "email": "examples@you.edu",
   "gender": "F"
 }
-
 // Write your intern objects here:
 
+const studentMitzi = {
+  id: 1,
+  name: "Mitzi",
+  email: "mmelloy0@psu.edu",
+  gender: "F",
+}
+
+const studentKennan = {
+  "id": 2,
+  name: "Kennan",
+  email: "kdiben1@tinypic.com",
+  gender: "M",
+  speak: function(){
+    console.log("Hello, my name is Kennan!")
+  }
+}
+
+const studentKeven = {
+  id: 3,
+  name: "Keven",
+  email: "kmummery2@wikimedia.org",
+  gender: "M ",
+}
+
+const studentGannie = {
+  id: 4,
+  name: "Gannie",
+  email: "gmartinson3@illinois.edu",
+  gender: "M",
+}
+
+const studentAntonietta = {
+  id: 5,
+  name: "Antoniette",
+  email: "adaine5@samsung.com",
+  gender: "F",
+  multiplyNums: function(num1, num2){
+    return num1 * num2
+  }
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
 
+console.log(studentMitzi)
+
 // Kennan's ID
+
+console.log(studentKennan.id)
 
 // Keven's email
 
+console.log(studentKeven.email)
+
 // Gannie's name
 
+console.log(studentGannie.name)
+
 // Antonietta's Gender
+
+console.log(studentAntonietta.gender)
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
 
+console.log(studentKennan.speak());
+
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+console.log(studentAntonietta.multiplyNums(3,2))
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
